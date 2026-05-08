4f1eb2be58b919358ef1fcc7ce54ebc80a32a445
sakurayung <fukurumiyuta13@gmail.com>
2026-05-08 09:20:08 +0800

    genesis

---
+++ apps/web/src/components/user-menu.tsx
@@ -0,0 +1,49 @@
+import { api } from "@project-construction/backend/convex/_generated/api";
+import { Button } from "@project-construction/ui/components/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@project-construction/ui/components/dropdown-menu";
+import { useNavigate } from "@tanstack/react-router";
+import { useQuery } from "convex/react";
+
+import { authClient } from "@/lib/auth-client";
+
+export default function UserMenu() {
+  const navigate = useNavigate();
+  const user = useQuery(api.auth.getCurrentUser);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger render={<Button variant="outline" />}>{user?.name}</DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-card">
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>{user?.email}</DropdownMenuItem>
+          <DropdownMenuItem
+            variant="destructive"
+            onClick={() => {
+              authClient.signOut({
+                fetchOptions: {
+                  onSuccess: () => {
+                    navigate({
+                      to: "/dashboard",
+                    });
+                  },
+                },
+              });
+            }}
+          >
+            Sign Out
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
